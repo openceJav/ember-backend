5f4a921559705726dc9bf283ad97c22b5624c76b
Muhammad Bilal Khan <lofi.audit@gmail.com>
2023-12-24 02:33:37 -0500

    Initial Commit

---
+++ src/main/java/org/ember/emberbackend/EmberBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ember.emberbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmberBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmberBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ember/emberbackend/EmberBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ember.emberbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmberBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
